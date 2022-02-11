c523e4197d04ed952034574820aa9bd150b81c13
Javier Felipe Villareal Cruz <jvillarrealc@unbosque.edu.co>
2022-02-10 22:39:20 -0800

    cambios en la clase estudiante

--- src/co/edu/unbosque/model/Estudiante.java
+++ src/co/edu/unbosque/model/Estudiante.java
@@ -2,4 +2,21 @@ package co.edu.unbosque.model;
 
 public class Estudiante {
 
+
+
+	 public static void main(String[] args) {
+			String carrera;
+			int edad;
+			String género;
+			
+		carrera = "ingenieria sistemas";
+		edad = 18;
+		género = "masculino";
+		
+		System.out.println("los datos del estudiante son: "
+				+ "\n carrera:"+carrera+" "
+						+ "\n edad: "+edad+" "
+								+ "\n género: "+género);
+		// TODO Auto-generated constructor stub
+	}
 }
