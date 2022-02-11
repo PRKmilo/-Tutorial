03fdbd01668373bcf490171f51bbba357a3a8a9a
Javier Felipe Villareal Cruz <jvillarrealc@unbosque.edu.co>
2022-02-11 12:23:48 -0800

    cambio de clase

--- src/co/edu/unbosque/model/Estudiante.java
+++ src/co/edu/unbosque/model/Estudiante.java
@@ -3,20 +3,45 @@ package co.edu.unbosque.model;
 public class Estudiante {
 
 
-
-	 public static void main(String[] args) {
-			String carrera;
-			int edad;
-			String género;
-			
-		carrera = "ingenieria sistemas";
-		edad = 18;
-		género = "masculino";
+	private String nombre;
+	private String carrera;
+	private int edad;
+	private String género;
+	public Estudiante() {
 		
-		System.out.println("los datos del estudiante son: "
-				+ "\n carrera:"+carrera+" "
-						+ "\n edad: "+edad+" "
-								+ "\n género: "+género);
+		this.carrera = carrera;
+		this.edad = edad;
+		this.género = género;
+		this.nombre = nombre;
+		
+		//System.out.println("los datos del estudiante son: "
+		//	+ "\n carrera:"+carrera+" "
+		//			+ "\n edad: "+edad+" "
+		//					+ "\n género: "+género);
 		// TODO Auto-generated constructor stub
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getGénero() {
+		return género;
+	}
+	public void setGénero(String género) {
+		this.género = género;
 	}
 }
